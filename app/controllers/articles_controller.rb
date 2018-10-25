f3399ffd873688ebe343fb8a74b379da9cad61ab
Danilosh <danilosh45@gmail.com>
2018-10-25 23:00:10 +0200

    Primer etapa de arquitectura rest backend para plataforma

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+	before_action :authenticate_user!, except: [:show,:index]
+	before_action :set_article, except: [:index,:new, :create]
+	def index
+		@articles =	Article.paginate(page: params[:page],per_page:5).publicados.ultimos
+
+		#obtines todos los registros 
+	end
+	def show
+		@article.update_visits_count
+		@comment = Comment.new
+	end
+	def new
+		@article = Article.new
+		@categories = Category.all
+	end
+
+	def edit
+	end
+
+	def create
+		@article = current_user.articles.new(article_params)
+		@article.categories = params[:categories]
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+	def destroy
+		
+		@article.destroy
+		redirect_to articles_path
+	end
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+	end
+	def publish
+		@article.publish!
+		redirect_to @article
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title,:body,:cover, :categories)
+		
+	end
+
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def dashboard
+  	@articles = Article.all
+  end
+end
